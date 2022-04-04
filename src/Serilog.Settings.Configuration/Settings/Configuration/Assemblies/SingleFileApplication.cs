8edbcb24fbaa03af72943158d4c06c4079b0a0ac
Cédric Luthi <cedric.luthi@gmail.com>
2022-04-04 19:57:54 +0200

    Dangerously hacky way to get the deps.json offset + size

--- src/Serilog.Settings.Configuration/Settings/Configuration/Assemblies/SingleFileApplication.cs
+++ src/Serilog.Settings.Configuration/Settings/Configuration/Assemblies/SingleFileApplication.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.MemoryMappedFiles;
+using System.Text.RegularExpressions;
 
 namespace Serilog.Settings.Configuration.Assemblies
 {
@@ -103,6 +105,59 @@ namespace Serilog.Settings.Configuration.Assemblies
             }
 
             return -1;
+        }
+
+        public static Stream GetDepsJsonStream_dangerously_hacky()
+        {
+            var appHostPath = Process.GetCurrentProcess().MainModule?.FileName;
+            if (appHostPath == null)
+            {
+                return null;
+            }
+
+            var depsJsonRegex = new Regex(@"DepsJson Offset:\[([0-9a-fA-F]+)\] Size\[([0-9a-fA-F]+)\]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var startInfo = new ProcessStartInfo(appHostPath)
+            {
+                EnvironmentVariables =
+                {
+                    ["COREHOST_TRACE"] = "1",
+                    ["COREHOST_TRACE_VERBOSITY"] = "3",
+                },
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+            };
+
+            using var process = new Process { StartInfo = startInfo };
+            long? depsJsonOffset = null;
+            long? depsJsonSize = null;
+            process.ErrorDataReceived += (sender, args) =>
+            {
+                var match = depsJsonRegex.Match(args.Data ?? "");
+                if (match.Success)
+                {
+                    var p = (Process)sender;
+                    p.CancelErrorRead();
+                    p.Kill();
+                    depsJsonOffset = Convert.ToInt64(match.Groups[1].Value, 16);
+                    depsJsonSize = Convert.ToInt64(match.Groups[2].Value, 16);
+                }
+            };
+
+            process.Start();
+            process.BeginErrorReadLine();
+            if (!process.WaitForExit(1000))
+            {
+                process.Kill();
+            }
+
+            if (depsJsonOffset != null && depsJsonSize != null)
+            {
+                using var appHostFile = MemoryMappedFile.CreateFromFile(appHostPath, FileMode.Open, null, 0, MemoryMappedFileAccess.Read);
+                return appHostFile.CreateViewStream(depsJsonOffset.Value, depsJsonSize.Value, MemoryMappedFileAccess.Read);
+            }
+
+            return null;
         }
     }
 }
